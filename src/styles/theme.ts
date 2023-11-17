24fb936a8924cb85533304a739472ea9d2215d48
BadMastropiero <mastropierro6@gmail.com>
2023-11-17 11:42:53 -0500

    Add custom theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,53 @@
+export const theme = {
+  colors: {
+    primary: {
+      base: '#f02672',
+      contrast: '#fff',
+    },
+    secondary: {
+      base: '#fff',
+      contrast: '#000',
+    },
+    darker: {
+      base: '#000',
+      contrast: '#fff',
+    },
+    lighter: {
+      base: '#fff',
+      contrast: '#000',
+    },
+    success: {
+      base: '#4caf50',
+      contrast: '#fff',
+    },
+    error: {
+      base: '#f44336',
+      contrast: '#fff',
+    },
+  },
+  layout: {
+    maxWidth: '1400px',
+    maxWidthS: '600px',
+    vGap: '10px',
+    hGap: '10px',
+  },
+  shadows: {
+    base: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+    hover: '0 10px 15px -3px rgba(0, 0, 0, 0.2), 0 4px 6px -2px rgba(0, 0, 0, 0.1)',
+  },
+  fontSizes: {
+    xl: '30px',
+    l: '25px',
+    m: '20px',
+    s: '16px',
+    sx: '14px',
+  },
+  borderRadius: {
+    base: '0px',
+    card: '12px',
+  },
+  easings: {
+    wiggle: 'cubic-bezier(0.74, -0.6, 0.225, 1.59)',
+    base: 'cubic-bezier(0.075, 0.82, 0.165, 1)',
+  },
+};
